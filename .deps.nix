81c8cbe9c4c85c752390ea7ed66be5db27cdb8d1
haruki7049 <tontonkirikiri@gmail.com>
2026-06-11 12:12:56 +0900

    Fix deps.nix

--- .deps.nix
+++ .deps.nix
@@ -1,14 +1,14 @@
 {
   linkFarm,
-  fetchzip,
+  fetchurl,
 }:
 
 linkFarm "zig-packages" [
   {
-    name = "riff_zig-1.1.2-J7H0A0lxTADPiTM0n8qFOJER78SXIXJTG63AjeC768FG";
-    path = fetchzip {
+    name = "riff_zig-1.1.2-J7H0A0lxTADPiTM0n8qFOJER78SXIXJTG63AjeC768FG.tar.gz";
+    path = fetchurl {
       url = "https://github.com/haruki7049/RIFF.zig/archive/refs/tags/1.1.2.tar.gz";
-      hash = "sha256-JNjOR4TvlPvJBGXE7zZ59XbgjUnnB9RPsEGMqMCJGAU=";
+      hash = "sha256-dtosAEQFVNuv24XuxVF1tUR91vcL5mRAARN/Ep3pJsQ";
     };
   }
 ]
